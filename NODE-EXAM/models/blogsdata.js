5f575c0ad5e94153c9420c4e6b6907738dc30282
ayushi bhatti <ayushibhatti2209@gmail.com>
2025-01-11 15:43:52 +0530

    c

---
+++ NODE-EXAM/models/blogsdata.js
@@ -0,0 +1,30 @@
+const mongoose=require('mongoose')
+
+
+const userschama=mongoose.Schema({
+    description:{
+        type:String,
+        require:true
+    },
+    name:{
+        type:String,
+        require:true
+    },
+    image:{
+        type:String,
+        require:true
+    },
+    price:{
+        type:String,
+        require:true
+    },
+    qty:{
+        type:String,
+        require:true
+    },
+
+})
+
+const blogeuser =mongoose.model('bloguser',userschama)
+
+module.exports=blogeuser    
